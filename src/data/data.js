f11104132b96f82899d18569614d9f03690ebe7d
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-25 18:38:27 +0330

    add assets to the data.js to use it later

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import headerimg from "../assets/header.png";
+import hightlightimg from "../assets/hightlightimg.png";
+import sneakershoe from "../assets/sneaker.png";
+
+import clip from "../assets/video/clip.mp4";
+import vcover1 from "../assets/video/vcover1.png";
+import vcover2 from "../assets/video/vcover2.png";
+import vcover3 from "../assets/video/vcover3.png";
+
+import psale1 from "../assets/nike-air-red.png";
+import psale2 from "../assets/nike-adapt-bb.png";
+import psale3 from "../assets/nike-adapt-bb-smart.png";
+
+import product1 from "../assets/product1.png";
+import product2 from "../assets/product2.png";
+import product3 from "../assets/product3.png";
+import product4 from "../assets/product4.png";
+import product5 from "../assets/product5.png";
+import product6 from "../assets/product6.png";
+import product7 from "../assets/product7.png";
+import product8 from "../assets/product8.png";
+import product9 from "../assets/product9.png";
+import product10 from "../assets/product10.png";
+import product11 from "../assets/product11.png";
+import product12 from "../assets/product12.png";
+
+
+import facebook from "../assets/facebook.svg";
+import instagram from "../assets/instagram.svg";
+import twitter from "../assets/twitter.svg";
+import youtube from "../assets/youtube.svg";
+import messenger from "../assets/messenger.svg";
+
+const headerapi = {
+  title: "Play With Electric Nike",
+  subtitle: "Adapt 2.0 Sneakers",
+  img: headerimg,
+  btntext: "Explore Product",
+  videos: [
+    { imgsrc: vcover1, clip: clip },
+    { imgsrc: vcover2, clip: clip },
+    { imgsrc: vcover3, clip: clip },
+  ],
+  sociallinks: [
+    { icon: facebook },
+    { icon: messenger },
+    { icon: instagram },
+    { icon: twitter },
+    { icon: youtube },
+  ],
+};
