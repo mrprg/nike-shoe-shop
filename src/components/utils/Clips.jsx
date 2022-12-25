035baf21cb0525a3093840c2a945624316cb2f5a
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-25 20:27:50 +0330

    make the clips container responsive

--- src/components/utils/Clips.jsx
+++ src/components/utils/Clips.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Clips = ({ clip, imgsrc, }) => {
   return (
     <>
-        <div className='relative h-28 w-32 rounded-xl overflow-hidden group cursor-pointer transition-all duration-300'>
+        <div className='relative h-28 w-32 rounded-xl overflow-hidden group cursor-pointer transition-all duration-300 lg:w-28 md:w-24 sm:w-16 lg:h-24 md:h-20 sm:h-14'>
             <img 
             src={imgsrc}
             alt="image"
