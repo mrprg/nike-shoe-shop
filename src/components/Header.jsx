a2fdeb8b3e8a0b9fb516899f6badf116cfa4afb7
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-25 19:07:12 +0330

    styling header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Header = ({ headerapi: { title, subtitle, img, btntext, videos, sociallinks } }) => {
   return (
     <>
-        <div>
-            <div></div>
-            <div>
+        <div className='relative h-auto w-auto flex flex-col'>
+            <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] 
+            md:h-[65vh] sm:h-[55vh] w-auto absolute 
+            top-0 left-0 right-0 opacity-100 z-10'></div>
+            <div className='relative opacity-100 z-20 grid 
+            items-center justify-items-center nike-container'>
                 <div>
                     <h1>{title}</h1>
                     <h1>{subtitle}</h1>
@@ -14,10 +17,10 @@ const Header = ({ headerapi: { title, subtitle, img, btntext, videos, sociallink
                     <div></div>
                 </div>
                 <div>
-                    <Img 
-                    src={img}
-                    alt='Nike'
-                    className=''
+                    <img 
+                        src={img}
+                        alt='Nike'
+                        className=''
                     />
                 </div>
             </div>
