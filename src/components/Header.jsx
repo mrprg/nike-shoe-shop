e63810f07f3e72987dc22347e651ff0899a50487
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-25 18:18:57 +0330

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+        <div>
+            <div></div>
+            <div>
+                <div>
+                    <h1></h1>
+                    <h1></h1>
+                    <button></button>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <Img 
+                    src=''
+                    alt='Nike'
+                    className=''
+                    />
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
