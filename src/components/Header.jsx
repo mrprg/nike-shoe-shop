cec2ac07448bb13e1322b5d90fe14e11cb4eede9
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-25 19:24:59 +0330

    continue styling header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,25 @@ const Header = ({ headerapi: { title, subtitle, img, btntext, videos, sociallink
             <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] 
             md:h-[65vh] sm:h-[55vh] w-auto absolute 
             top-0 left-0 right-0 opacity-100 z-10'></div>
+            
             <div className='relative opacity-100 z-20 grid 
             items-center justify-items-center nike-container'>
-                <div>
-                    <h1>{title}</h1>
-                    <h1>{subtitle}</h1>
-                    <button>{btntext}</button>
+                
+                <div className='grid items-center 
+                justify-items-center mt-28 md:mt-24'>
+
+                    <h1 className='text-6xl lg:text-5xl 
+                    md:text-4xl sm:text-3xl xsm:text-2xl 
+                    font-extrabold filter drop-shadow-sm
+                     text-slate-200'>{title}</h1>
+                    
+                    <h1 className='text-6xl lg:text-5xl
+                    md:text-4xl sm:text-3xl xsm:text-2xl
+                    font-extrabold filter drop-shadow-sm
+                    text-slate-200'>{subtitle}</h1>
+                    
+                    <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'>{btntext}</button>
+                    
                     <div></div>
                     <div></div>
                 </div>
