01a0bf06f435511d8d0f4e61110c1e6fa1380f74
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-27 01:23:15 +0330

    styling playicon

--- src/components/utils/Clips.jsx
+++ src/components/utils/Clips.jsx
@@ -10,8 +10,8 @@ const Clips = ({ clip, imgsrc, }) => {
             alt="image"
             className='inset-0 flex h-full w-full object-cover absolute top-0 left-0 right-0 rounded-xl opacity-100 z-10 transition-opacity duration-500'
             />
-            <div>
-              <PlayIcon className='icon-style' />
+            <div className='bg-white blur-effect-theme absolute top-11 left-11 lg:top-8 lg:left-9 sm:top-4 sm:left-5 right-0 opacity-100 z-[100] w-8 h-8 md:w-5 md:h-5 flex items-center justify-center rounded-full'>
+              <PlayIcon className='icon-style text-slate-900 md:w-3 md:h-3' />
             </div>
             <video
             autoPlay={true}
