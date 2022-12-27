e7982956e3d2f8fc5a7fcb38b4a3c445afb46ad1
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-27 11:57:50 +0330

    styling sales

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -5,13 +5,13 @@ import Title from './utils/Title';
 const Sales = ({ endpoint: { title, items } }) => {
   return (
     <>
-      <div>
-        <Title title={title} />
-      </div>
-      <div>
-        {items?.map((item, i) => (
-          <Item {...item} key={i} />
-        ))}
+      <div className='nike-container'>
+          <Title title={title} />
+        <div className='grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7'>
+          {items?.map((item, i) => (
+            <Item {...item} key={i} />
+            ))}
+        </div>
       </div>
     </>
   )
