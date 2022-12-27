ebb2d3d88f845b4962f2fee29355da77013d73fa
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-27 11:13:27 +0330

    main structre of item components

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
 
 const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <div>
+            <div>
+              <h1>{price}</h1>
+            </div>
+            <div>
+              <h1><StarIcon className='icon-style text-slate-900' />{rating}</h1>
+            </div>
+          </div>
+          <div>
+            <button type='button' className=''><ShoppingBagIcon className='icon-style text-slate-900'/></button>
+            <button type='button' className=''>{btn}</button>
+          </div>
+        </div>
+        <div><img
+          src={img}
+          alt='image'
+          className=''
+         />
+         </div>
+      </div>
     </>
   )
 }
