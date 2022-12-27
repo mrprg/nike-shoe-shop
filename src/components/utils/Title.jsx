7a4c95df197b38bbb712c0b5100cc27b7327e600
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-27 11:18:38 +0330

    styling Title

--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Title = ({ title }) => {
   return (
     <>
-      <div>
-        <h1>{title}</h1>
+      <div className='grid items-center'>
+        <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter drop-shadow-lg'>{title}</h1>
       </div>
     </>
   )
