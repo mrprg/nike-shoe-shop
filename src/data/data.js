351958344c4fea95245065eb0584bc8f30811b9b
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-30 21:23:42 +0330

    add new data

--- src/data/data.js
+++ src/data/data.js
@@ -226,3 +226,21 @@ export const topratedsales = {
     },
   ],
 };
+
+const highlight = {
+  heading: "HIGHLIGHTS",
+  title: "NIKE AIR WITH LIMITLESS CHOICES",
+  text: "Our Purpose is to move the world forward. We take action by building community, protecting our planet and increasing access to sport.",
+  btn: "Explore More",
+  url: "https://www.nike.com/launch/t/nocta-hot-step-black-gold",
+  img: hightlightimg,
+};
+
+const sneaker = {
+  heading: "FEATURED",
+  title: "NIKE SNEAKERS AIR LANCING SHOES",
+  text: "The radiance lives on Nike Sneakers Air Lancing Shoes, the basket ball OG that puts a fresh spin on what you know best: durably stitched overlays, clean finishes and the perfect amount of flash to make you shine.",
+  btn: "Explore More",
+  url: "https://sneakernews.com/2022/03/21/nike-lebron-2-retro-white-midnight-navy-varsity-crimson-dr0826-100/",
+  img: sneakershoe,
+};
