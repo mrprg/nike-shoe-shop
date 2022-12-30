f323fa2af50050e61386fc2fea67ae02702e86a4
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-30 22:28:43 +0330

    styling the FlexContent Component

--- src/components/FlexContent.jsx
+++ src/components/FlexContent.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 
-const FlexContent = () => {
+const FlexContent = ({ endpoint : { heading, title, text, btn, url, img} }) => {
   return (
     <>
-        <div>
-            <div>
-                <h1></h1>
-                <h1></h1>
-                <p></p>
-                <a>
+        <div className={`flex items-center justify-between
+            lg:flex-col lg:justify-center nike-container
+        `}>
+            <div className='max-w-lg lg:max-w-none w-full
+                md:text-center grid items-center lg:justify-items-center
+            '>
+                <h1 className='text-4xl sm:text-3xl font-bold text-gradiant'>{heading}</h1>
+                <h1 className='text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-bold text-slate-900 filter drop-shadow-lg'>{title}</h1>
+                <p className='xl: text-sm my-4 text-slate-900'>{text}</p>
+                <a href={url} className='flex items-center' target={"_blank"} role="button">
                     <button type='button'
-                        className=''
+                        className='button-theme bg-slate-900 shadow-slate-900 text-slate-100 py-1.5'
                     >
-
+                            {btn}
                     </button>
                 </a>
             </div>
-            <div>
+            <div className='flex items-center justify-center max-w-xl relative lg:max-w-none w-full'>
                 <img
-                    src=''
-                    alt=''
-                    className=''
+                    src={img}
+                    alt='image/'
+                    className={`w-auto object-fill transition-theme`}
                 />
             </div>
         </div>
