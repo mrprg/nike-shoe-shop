741b1d2cb6e58c46ab739b7bdbfdf98bdbec32f5
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-01 18:58:40 +0330

    structure Srories component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import Title from './utils/Title';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import { ClockIcon, HeartIcon } from '@heroicons/react/24/solid';
 const Stories = ({ story: { title, news } }) => {
   return (
     <>
-        
+        <div className='nike-container mb-11'>
+              <Title title={title} />  
+              <div>
+                    <Splide>
+                        {news.map((val, i) => (
+                            <SplideSlide key={i} className='mb-0.5'>
+                                <div>
+                                    <img src={val.img} alt='image' className=''
+                                    />
+                                    <div>
+                                        <div>
+                                            <HeartIcon className='icon-style' /><span>{val.like}</span>
+                                        </div>
+                                        <div>
+                                            <ClockIcon className='icon-style' /><span>{val.time}</span>
+                                        </div>
+                                        <div>
+                                            <HeartIcon className='icon-style' /><span>{val.by}</span>
+                                        </div>
+                                        <div>
+                                            <h1>{val.title}</h1>
+                                            <p>{val.text}</p>
+                                        </div>
+                                        <div>
+                                            <a href={val.url}>{val.btn}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </SplideSlide>
+                        ))}
+                    </Splide>
+              </div>
+        </div>
     </>
   )
 }
