5f2db92722e3ef40e8208f4b152070f78230f3b1
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-01 19:49:14 +0330

    styling Stories Component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,46 +1,87 @@
-import React from 'react';
-import Title from './utils/Title';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import { ClockIcon, HeartIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
+import { HashtagIcon, HeartIcon } from "@heroicons/react/24/solid";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import Title from "./utils/Title";
+import { truncate } from "lodash";
+
 const Stories = ({ story: { title, news } }) => {
+  const splideOptions = {
+    perPage: 4,
+    perMove: 1,
+    type: "loop",
+    rewind: true,
+    keyboard: "global",
+    gap: "1rem",
+    pagination: false,
+    padding: "2rem",
+    breakpoints: {
+      1200: { perPage: 3 },
+      991: { perPage: 2.3 },
+      768: { perPage: 2 },
+      500: { perPage: 1.3 },
+      425: { perPage: 1 },
+    },
+  };
   return (
     <>
-        <div className='nike-container mb-11'>
-              <Title title={title} />  
-              <div>
-                    <Splide>
-                        {news.map((val, i) => (
-                            <SplideSlide key={i} className='mb-0.5'>
-                                <div>
-                                    <img src={val.img} alt='image' className=''
-                                    />
-                                    <div>
-                                        <div>
-                                            <HeartIcon className='icon-style' /><span>{val.like}</span>
-                                        </div>
-                                        <div>
-                                            <ClockIcon className='icon-style' /><span>{val.time}</span>
-                                        </div>
-                                        <div>
-                                            <HeartIcon className='icon-style' /><span>{val.by}</span>
-                                        </div>
-                                        <div>
-                                            <h1>{val.title}</h1>
-                                            <p>{val.text}</p>
-                                        </div>
-                                        <div>
-                                            <a href={val.url}>{val.btn}</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </SplideSlide>
-                        ))}
-                    </Splide>
-              </div>
+      <div className="nike-container mb-11">
+        <Title title={title} />
+        <div className="mt-7">
+          <Splide options={splideOptions}>
+            {news.map((val, i) => (
+              <SplideSlide key={i} className="mb-0.5">
+                <div className="relative grid items-center gap-4 pb-2 rounded-lg shadow shadow-slate-200 ring-1 ring-slate-200">
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={val.img}
+                      alt={`img/story/${i}`}
+                      className="w-full h-auto object-cover shadow-md shadow-slate-200 rounded-tl-lg rounded-tr-lg"
+                    />
+                  </div>
+                  <div className="flex items-center justify-between w-full px-4">
+                    <div className="flex items-center gap-0.5">
+                      <HeartIcon className="icon-style fill-red-500 w-5 h-5" />
+                      <span className="text-xs font-bold">{val.like}</span>
+                    </div>
+                    <div className="flex items-center gap-0.5">
+                      <ClockIcon className="icon-style w-4 h-4 fill-black" />
+                      <span className="text-xs font-bold">{val.time}</span>
+                    </div>
+                    <div className="flex items-center gap-0.5">
+                      <HashtagIcon className="icon-style fill-blue-600" />
+                      <span className="text-xs font-bold text-blue-600">
+                        {val.by}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="grid items-center justify-items-start px-4">
+                    <h1 className="text-base font-semibold lg:text-sm">
+                      {val.title}
+                    </h1>
+                    <p className="text-sm text-justify lg:text-xs">
+                      {truncate(val.text, { length: 175 })}
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-center px-4 w-full">
+                    <a
+                      href={val.url}
+                      target="_blank"
+                      role={"button"}
+                      className="w-full bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5 button-theme"
+                    >
+                      {val.btn}
+                    </a>
+                  </div>
+                </div>
+              </SplideSlide>
+            ))}
+          </Splide>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Stories;
