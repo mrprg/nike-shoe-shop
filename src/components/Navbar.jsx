ae5afd3ef02fa4cd36d11eb619f74e2539983579
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-02 20:53:51 +0330

    srtucture of Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
+import { HeartIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <header>
+            <nav>
+                <div>
+                    <img 
+                        src=''
+                        alt=''
+                        className=''
+                    />
+                </div>
+                <ul>
+                    <li>
+                        <MagnifyingGlassIcon className='icon-style'/>
+                    </li>
+                    <li>
+                        <HeartIcon />
+                    </li>
+                    <li>
+                        <button type='button' className=''>
+                            <ShoppingBagIcon className='icon-style' />
+                            <div>0</div>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
   )
 }
 
