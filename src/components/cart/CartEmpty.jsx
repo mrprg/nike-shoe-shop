44670f3f1eaf7f7bfcdd350e8f3ab5835f65381b
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-06 16:19:56 +0330

    fix some styles

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import emptybag from "../../assets/emptybag.png";
-import { ArrowLeftIcon } from '@heroicons/react/24/solid'
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const CartEmpty = () => {
   return (
     <>
       <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
-        <img src={emptybag} alt="empty-bag" className="w-40 lg:w-36 sm:w-26 h-auto object-fill transition-all hover:scale-110 duration-300" />
-        <button type="button" className="button-theme bg-gradiant-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110">
+        <img
+          src={emptybag}
+          alt="emptybag/img"
+          className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110"
+        />
+        <button
+          type="button"
+          className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
+        >
           <ArrowLeftIcon className="w-5 h-5 text-slate-900" />
           <span className="">Back To Nike Store</span>
         </button>
