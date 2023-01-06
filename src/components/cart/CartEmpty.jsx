9d39f3690323f26670c4a34cb94b1b5256327761
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-06 16:10:01 +0330

    Styling CartEmty Component

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import emptybag from "../../assets/emptybag.png";
+import { ArrowLeftIcon } from '@heroicons/react/24/solid'
 
 const CartEmpty = () => {
   return (
-    <div>CartEmpty</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
+        <img src={emptybag} alt="empty-bag" className="w-40 lg:w-36 sm:w-26 h-auto object-fill transition-all hover:scale-110 duration-300" />
+        <button type="button" className="button-theme bg-gradiant-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110">
+          <ArrowLeftIcon className="w-5 h-5 text-slate-900" />
+          <span className="">Back To Nike Store</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default CartEmpty
+export default CartEmpty;
