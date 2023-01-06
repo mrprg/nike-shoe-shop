f52b0fef2ac8f4f62f3907454d49760f71c65cdf
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-06 17:06:10 +0330

    ADD NEW FILES (REDUX TOOLKIT)

---
+++ src/app/CartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartState: false,
+}
+
+const CartSlice = createSlice({
+    initialState,
+    name:"cart",
+    reducers: {
+        
+    }
+})
