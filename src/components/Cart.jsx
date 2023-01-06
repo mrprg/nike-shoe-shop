4efc1b2feef74e17b5ccb1dec1bf3b336bd4a2b9
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-06 14:58:53 +0330

    styling cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,8 +6,8 @@ import CartItem from './cart/CartItem';
 const Cart = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[250]`}>
+        <div className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}>
           <CartCount />
           <CartEmpty />
           <CartItem />
