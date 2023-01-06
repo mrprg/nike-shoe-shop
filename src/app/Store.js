e0322205799b00946e23a38d468f6291f73f2f31
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-06 17:11:52 +0330

    make a store by using configure store

---
+++ src/app/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./CartSlice.js";
+
+const Store = configureStore({ 
+        reducer: {
+            cart: CartSlice
+        }
+ });
